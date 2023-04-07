cb22340e9565cb19f56a2314d22611233f6abb44
Madhumitha S <87703806+Madhumitha03S@users.noreply.github.com>
2023-04-07 09:36:44 +0530

    Create Sudoku.java

---
+++ 20bps1016-cse1007-lab04/Sudoku.java
@@ -0,0 +1,81 @@
+// Solve sudoku game for any n x n grid using java.
+
+public class Sudoku {
+
+    private int[][] board;
+
+    public Sudoku(int[][] board) {
+        this.board = board;
+    }
+
+    public boolean solve() {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= 9; num++) {
+                        if (isSafe(row, col, num)) {
+                            board[row][col] = num;
+                            if (solve()) {
+                                return true;
+                            } else {
+                                board[row][col] = 0;
+                            }
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isSafe(int row, int col, int num) {
+        // Check row and column
+        for (int i = 0; i < 9; i++) {
+            if (board[row][i] == num || board[i][col] == num) {
+                return false;
+            }
+        }
+
+        // Check sub-grid
+        int rowStart = row - row % 3;
+        int colStart = col - col % 3;
+        for (int i = rowStart; i < rowStart + 3; i++) {
+            for (int j = colStart; j < colStart + 3; j++) {
+                if (board[i][j] == num) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public void printSolution() {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] board = {
+                { 5, 3, 0, 0, 7, 0, 0, 0, 0 },
+                { 6, 0, 0, 1, 9, 5, 0, 0, 0 },
+                { 0, 9, 8, 0, 0, 0, 0, 6, 0 },
+                { 8, 0, 0, 0, 6, 0, 0, 0, 3 },
+                { 4, 0, 0, 8, 0, 3, 0, 0, 1 },
+                { 7, 0, 0, 0, 2, 0, 0, 0, 6 },
+                { 0, 6, 0, 0, 0, 0, 2, 8, 0 },
+                { 0, 0, 0, 4, 1, 9, 0, 0, 5 },
+                { 0, 0, 0, 0, 8, 0, 0, 7, 9 }
+        };
+        Sudoku solver = new Sudoku(board);
+        if (solver.solve()) {
+            solver.printSolution();
+        } else {
+            System.out.println("No solution found");
+        }
+    }
+}
